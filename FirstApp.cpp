bdb4963372fe4f1ce633b1d9cdb0fd60d361210f
fritzdewit <fritzdewit@gmail.com>
2026-01-28 18:11:56 -0700

    First release of FirstApp

---
+++ FirstApp.cpp
@@ -0,0 +1,13 @@
+#include "FirstApp.h"
+FirstApp::FirstApp()
+{
+}
+FirstApp::~FirstApp(void)
+{
+}
+void FirstApp::Greeting()
+{
+}
+void FirstApp::Farewell()
+{
+}
---
+++ FirstApp.h
@@ -0,0 +1,11 @@
+#ifndef _FIRST_APP_H
+#define _FIRST_APP_H
+class FirstApp {
+public:
+FirstApp(void);
+~FirstApp(void);
+public:
+void Greeting();
+void Farewell(void);
+};
+#endif
